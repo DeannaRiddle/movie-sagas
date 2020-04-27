ccc277046290bea0d3783b4a87be9390754e6669
Deanna Riddle <lriddle.deanna@gmail.com>
2020-04-27 12:56:27 -0500

    setting up componetns

---
+++ src/components/DetailsPage/DetailsPage.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class DetailsPage extends Component{
+
+    render ( ) {
+    
+    }
+    return(
+        
+    )
+    
+    }
+
+
+
+
+
+
+
+export default connect()(DetailsPage);
